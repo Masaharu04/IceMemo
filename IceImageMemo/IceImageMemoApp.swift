e3dadea13dbd53c2c95bd3cba0d694fdccab79f1
Masaharu04 <masaharu7532@icloud.com>
2023-07-24 21:06:20 +0900

    Initial Commit

---
+++ IceImageMemo/IceImageMemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  IceImageMemoApp.swift
+//  IceImageMemo
+//
+//  Created by Masaharu on 2023/07/24.
+//
+
+import SwiftUI
+
+@main
+struct IceImageMemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
