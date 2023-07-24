2844ff7ed7fd81011ad22607e829e62a44e7d774
Masaharu04 <masaharu7532@icloud.com>
2023-07-25 02:26:19 +0900

    hello

---
+++ IceImageMemo/GridView.swift
@@ -0,0 +1,20 @@
+//
+//  GridView.swift
+//  IceImageMemo
+//
+//  Created by Masaharu on 2023/07/25.
+//
+
+import SwiftUI
+
+struct GridView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct GridView_Previews: PreviewProvider {
+    static var previews: some View {
+        GridView()
+    }
+}
---
+++ IceImageMemo/showImage.swift
@@ -0,0 +1,20 @@
+//
+//  showImage.swift
+//  IceImageMemo
+//
+//  Created by Masaharu on 2023/07/25.
+//
+
+import SwiftUI
+
+struct showImage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct showImage_Previews: PreviewProvider {
+    static var previews: some View {
+        showImage()
+    }
+}
