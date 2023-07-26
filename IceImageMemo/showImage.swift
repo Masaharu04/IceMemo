c3995d897c50e15dad8abc5fe2ee177500b04489
A3426L <mikan7738@gmail.com>
2023-07-26 12:51:10 +0900

    [edit]aspect

--- IceImageMemo/showImage.swift
+++ IceImageMemo/showImage.swift
@@ -58,7 +58,7 @@ struct showImage: View {
             if let view_image = select_image{
                 Image(uiImage:view_image)
                     .resizable()
-                    .aspectRatio(3/4, contentMode: .fit)
+                    .aspectRatio(3024/4032, contentMode: .fit)
                     .scaledToFill()
                     .cornerRadius(20)
                     .onTapGesture {
