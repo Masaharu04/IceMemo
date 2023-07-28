cf263a33f0ac646cab3afed153faf24f2d4b91b1
A3426L <mikan7738@gmail.com>
2023-07-28 21:30:48 +0900

    [edit]

--- IceImageMemo/zoom.swift
+++ IceImageMemo/zoom.swift
@@ -6,3 +6,21 @@
 //
 
 import Foundation
+import CoreImage
+import CoreImage.CIFilterBuiltins
+
+func convertToMonochrome(image: UIImage) -> UIImage? {
+    if let ciImage = CIImage(image: image) {
+        let filter = CIFilter.colorMonochrome()
+        filter.inputImage = ciImage
+        filter.intensity = 1.0 
+
+        if let outputImage = filter.outputImage {
+            let context = CIContext()
+            if let cgImage = context.createCGImage(outputImage, from: outputImage.extent) {
+                return UIImage(cgImage: cgImage)
+            }
+        }
+    }
+    return nil // 変換に失敗した場合はnilを返します
+}
